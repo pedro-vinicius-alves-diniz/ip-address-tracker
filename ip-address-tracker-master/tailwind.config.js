5fabbc0c45e4849d67f67831844fa98d63ea0103
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-01-22 18:39:23 +0100

    start settings

--- ip-address-tracker-master/tailwind.config.js
+++ ip-address-tracker-master/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-gray': 'hsl(0, 0%, 17%)',
+        'dark-gray': 'hsl(0, 0%, 59%)'
+      },
+      backgroundImage: {
+        'mobile': 'url(./src/images/pattern-bg-mobile.png)',
+        'desktop': 'url(./src/images/pattern-bg-desktop.png)'
+      },
+      fontFamily: {
+        'rubik': ["Rubik", "serif"]
+      }
+    },
   },
   plugins: [],
 }
