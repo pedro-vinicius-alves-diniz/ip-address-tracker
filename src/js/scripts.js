d83c4b7142d5a1e9853b064859a762b521cc849c
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-01-24 15:59:03 +0100

    finished

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,8 @@ const ip = document.getElementById("ip");
 const loc = document.getElementById("location");
 const timezone = document.getElementById("timezone");
 const isp = document.getElementById("isp");
+const main = document.getElementById("main");
+let map
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
@@ -14,20 +16,46 @@ button.addEventListener("click", (event) => {
 
 async function require() {
   const response = await fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKeFy=at_wQDAcSbLE3632ELGZTmFDiF7nJC7D&ipAddress=192.212.174.101`
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_wQDAcSbLE3632ELGZTmFDiF7nJC7D&ipAddress=${inputIp.value}&domain=${inputIp.value}`
   );
 
-  const data = await response.json();
+  try {
+    const data = await response.json();
+    const IP = data.ip 
+    const country = data.location.country 
+    const city = data.location.city
+    let postalCode
+    const utc = data.location.timezone
+    const ISP = data.isp
 
-  console.log(data.ip);
-  ip.innerHTML = data.ip;
+    if(data.location.postalCode){
+      postalCode = data.location.postalCode 
+    }else{
+      postalCode = ""
+    }
 
-  console.log(data.location);
-  loc.innerHTML = data.location.city;
+    ip.innerHTML = IP;
+    loc.innerHTML = `${country}, ${city} ${postalCode}`;
+    timezone.innerHTML = `UTC ${utc}`;
+    isp.innerHTML = ISP;
 
-  console.log(data.location.timezone);
-  timezone.innerHTML = `UTC ${data.location.timezone}`;
+    addMap(data.location.lat, data.location.lng);
+  } catch {
+    console.log(Error);
+  }
+}
 
-  console.log(data.isp);
-  isp.innerHTML = data.isp;
+function addMap(lat, lng) {
+  if(map){
+    map.remove()
+  }
+
+  map = L.map("map").setView([lat, lng], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  var marker = L.marker([lat, lng]).addTo(map);
 }
